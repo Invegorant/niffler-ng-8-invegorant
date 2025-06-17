d172575dc10a7f74d3f0763ab3e0b6d505532983
Invegorant <kraisyvirus@yandex.ru>
2025-06-17 22:14:37 +0700

    finished homework 8.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -1,14 +1,40 @@
 package guru.qa.niffler.test.web;
 
+import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
+import guru.qa.niffler.jupiter.extension.StaticBrowserExtension;
+import guru.qa.niffler.model.Browser;
 import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.test.web.utils.BrowserConverterUtils;
+import guru.qa.niffler.test.web.utils.SelenideUtils;
+import guru.qa.niffler.utils.RandomDataUtils;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.EnumSource;
 
 @WebTest
 @Feature("Авторизация")
 public class LoginTest extends AbstractTest {
+
+    @RegisterExtension
+    private static final StaticBrowserExtension BROWSER_EXTENSION = new StaticBrowserExtension();
+    private final SelenideDriver chrome = new SelenideDriver(SelenideUtils.chromeConfig);
+
+    @ParameterizedTest
+    @EnumSource(Browser.class)
+    void auth_userShouldStayOnLoginPageAfterLoginWithBadCredentials(@ConvertWith(BrowserConverterUtils.class) SelenideDriver driver) {
+
+        BROWSER_EXTENSION.drivers().add(driver);
+        driver.open(LoginPage.URL);
+        new LoginPage(driver).setUsername(RandomDataUtils.randomUsername())
+                .setPassword("12345")
+                .submit();
+        assertError("Неверные учетные данные пользователя");
+    }
 
     @Test
     @DisplayName("Авторизация - Успешная авторизация")

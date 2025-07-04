d6bf66e0f28b13178f904d7d3d234d4b1e11493c
Invegorant <kraisyvirus@yandex.ru>
2025-07-04 14:07:52 +0700

    finished homework 10.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/SpendApiClient.java
@@ -1,44 +1,27 @@
 package guru.qa.niffler.service.impl;
 
 import guru.qa.niffler.api.SpendApi;
+import guru.qa.niffler.api.core.RestClient;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.service.SpendClient;
-import guru.qa.niffler.service.api.Execute;
 import io.qameta.allure.Step;
-import io.qameta.allure.okhttp3.AllureOkHttp3;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import org.jetbrains.annotations.NotNull;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 import java.util.UUID;
 
 @ParametersAreNonnullByDefault
-public class SpendApiClient implements SpendClient, Execute {
+public class SpendApiClient extends RestClient implements SpendClient {
 
-    private static final Config CFG = Config.getInstance();
+    private final SpendApi spendApi;
 
-    private final OkHttpClient client = new OkHttpClient.Builder()
-            .addNetworkInterceptor(
-                    new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            .addNetworkInterceptor(
-                    new AllureOkHttp3()
-                            .setRequestTemplate("http-request.ftl")
-                            .setResponseTemplate("http-response.ftl")
-            ).build();
-    private final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(CFG.spendUrl())
-            .client(client)
-            .addConverterFactory(JacksonConverterFactory.create())
-            .build();
-
-    private final SpendApi spendApi = retrofit.create(SpendApi.class);
-
+    public SpendApiClient() {
+        super(Config.getInstance().spendUrl());
+        this.spendApi = create(SpendApi.class);
+    }
 
     @Step("Create spend: {spend}")
     @NotNull

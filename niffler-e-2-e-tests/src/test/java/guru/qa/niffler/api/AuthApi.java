c009bfca03680b9c3ec1a675ecdef3351f25884b
Invegorant <kraisyvirus@yandex.ru>
2025-07-06 21:37:12 +0700

    finish homework 11.1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
@@ -7,29 +7,27 @@ import retrofit2.http.*;
 public interface AuthApi {
 
     @GET("oauth2/authorize")
-    Call<Void> authorize(
-            @Query("response_type") String responseType,
-            @Query("client_id") String clientId,
-            @Query("scope") String scope,
-            @Query(value = "redirect_uri", encoded = true) String redirectUri,
-            @Query("code_challenge") String codeChallenge,
-            @Query("code_challenge_method") String codeChallengeMethod);
-
-    @POST("oauth2/token")
-    @FormUrlEncoded
-    Call<JsonNode> token(
-            @Field("client_id") String clientId,
-            @Field(value = "redirect_uri", encoded = true) String redirectUri,
-            @Field("grant_type") String grantType,
-            @Field("code") String code,
-            @Field("code_verifier") String codeChallenge);
+    Call<Void> authorize(@Query("response_type") String responseType,
+                         @Query("client_id") String clientId,
+                         @Query("scope") String scope,
+                         @Query(value = "redirect_uri", encoded = true) String redirectUri,
+                         @Query("code_challenge") String codeChallenge,
+                         @Query("code_challenge_method") String codeChallengeMethod);
 
     @POST("login")
     @FormUrlEncoded
-    Call<Void> login(
-            @Field("username") String username,
-            @Field("password") String password,
-            @Field("_csrf") String csrf);
+    Call<Void> login(@Field("_csrf") String csrf,
+                     @Field("username") String username,
+                     @Field("password") String password);
+
+
+    @POST("oauth2/token")
+    @FormUrlEncoded
+    Call<JsonNode> token(@Field("code") String code,
+                         @Field(value = "redirect_uri", encoded = true) String redirectUri,
+                         @Field("code_verifier") String codeVerifier,
+                         @Field("grant_type") String grantType,
+                         @Field("client_id") String clientId);
 
     @GET("register")
     Call<Void> requestRegisterForm();

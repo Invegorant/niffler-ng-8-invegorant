02a36293630a21de29567a4a74d5a00fbd761535
Invegorant <kraisyvirus@yandex.ru>
2025-09-28 13:38:33 +0700

    finished wiremock tests

--- niffler-gateway/src/test/java/qa/niffler/controller/CategoriesControllerTest.java
+++ niffler-gateway/src/test/java/qa/niffler/controller/CategoriesControllerTest.java
@@ -1,63 +1,31 @@
 package qa.niffler.controller;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import qa.niffler.controller.jupiter.WiremockStubs;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class CategoriesControllerTest {
 
-    private final WireMockServer wiremock = new WireMockServer(
-            new WireMockConfiguration()
-                    .port(8093)
-                    .globalTemplating(true)
-    );
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void beforeEach() {
-        wiremock.start();
-    }
-
-    @AfterEach
-    void afterEach() {
-        wiremock.shutdown();
-    }
-
+    @WiremockStubs(value = {"getAllCategories.json"}, port = 8093)
     @Test
     void categoriesListShouldBeReturnedForCurrentUser() throws Exception {
-        final String fixtureUser = "bee";
-
-        wiremock.stubFor(WireMock.get(urlPathEqualTo("/internal/categories/all"))
-                .withQueryParam("username", equalTo(fixtureUser))
-                .withQueryParam("excludeArchived", equalTo("false"))
-                .willReturn(okJson(
-                        """
-                                    [
-                                      {"id":"{{randomValue type='UUID'}}","name":"Веселье","username":"{{request.query.username}}","archived":false},
-                                      {"id":"{{randomValue type='UUID'}}","name":"Магазины","username":"{{request.query.username}}","archived":true}
-                                    ]
-                                """
-                )));
+        final String fixtureUser = "duck";
 
         mockMvc.perform(get("/api/categories/all")
                         .with(jwt().jwt(c -> c.claim("sub", fixtureUser)))
@@ -71,4 +39,5 @@ class CategoriesControllerTest {
                 .andExpect(jsonPath("$[1].archived").value(true))
                 .andDo(print());
     }
+
 }
